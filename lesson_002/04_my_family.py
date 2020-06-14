fdd6616a84fc68e8550c1aba14ab8e95d7147200
Ruslan <rusikeabudana@gmail.com>
2020-06-15 00:39:18 +0600

    Выполнил первые 6 заданий 2 модуля

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -5,16 +5,25 @@
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 my_family = []
+my_family.append('me')
+my_family.append('mother')
+my_family.append('father')
+my_family.append('sister')
+my_family = my_family + ['brother', 'grandma', 'grandpa']
+
+
 
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    ['me', 171], ['mother', 165], ['father', 172], ['sister', 169], ['brother', 175], ['grandma', 158], ['grandpa', 170],
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-
+print('Рост отца -', my_family_height[2][1], 'см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+sum = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + my_family_height[4][1] + my_family_height[5][1] + my_family_height[6][1]
+print('Общий рост моей семьи - ', sum, 'см')

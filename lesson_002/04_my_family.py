bb19c6a2db394e545c88c8639e1b12ed69ffbb67
Ruslan <rusikeabudana@gmail.com>
2020-06-24 16:17:16 +0600

    обработал согласно требованиям!

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -4,21 +4,8 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-# TODO В такой ситуации, когда вы создаёте список и тут же добавляете в него
-#  элементы, лучше сразы сделать список с этими элементами. Вместо:
-#  my_list = []
-#  my_list.append(1)
-#  my_list.append(2)
-#  my_list.append(3)
-#  Нужно сделать:
-#  my_list = [1, 2, 3]
-my_family = []
-my_family.append('me')
-my_family.append('mother')
-my_family.append('father')
-my_family.append('sister')
-my_family = my_family + ['brother', 'grandma', 'grandpa']
 
+my_family = ['me', 'mother', 'father', 'sister', 'brother', 'grandma', 'grandpa', ]
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
@@ -31,9 +18,6 @@ my_family_height = [
 print('Рост отца -', my_family_height[2][1], 'см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
-# TODO Название sum зарезервированно для встроенной функции. Использование этого названия для
-#  имени переменной может вызывать проблемы.
-
-sum = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + \
-      my_family_height[4][1] + my_family_height[5][1] + my_family_height[6][1]
-print('Общий рост моей семьи - ', sum, 'см')
+total_height = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + \
+               my_family_height[4][1] + my_family_height[5][1] + my_family_height[6][1]
+print('Общий рост моей семьи - ', total_height, 'см')

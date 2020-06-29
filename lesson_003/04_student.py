085ac1d418416d782ea01b64247dda81408295f6
Ruslan <rusikeabudana@gmail.com>
2020-06-30 02:12:17 +0600

    04 - сделано

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -11,4 +11,18 @@
 
 educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+parents_help = 0
+month_counter = 1
+total_expenses = expenses
+coefficient = 1.03
+month_number = 10
+
+while month_counter < month_number:
+    expenses *= coefficient
+    total_expenses += expenses
+    month_counter += 1
+    print(month_counter, expenses, total_expenses, month_counter * educational_grant)
+
+parents_help = round(total_expenses - educational_grant * month_number, 2)
+
+print('Студенту необходимо попросить у родителей', parents_help, 'руб.')

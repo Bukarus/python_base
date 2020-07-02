150e23ec7b0b8e1f52d4f6bb95c3ff442a8d698f
Ruslan <rusikeabudana@gmail.com>
2020-07-02 23:40:07 +0600

    07 - стена из цветных кирпичей

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -29,4 +29,7 @@ for y in range(0, 600, 50):
         left_bottom = sd.get_point(x, y)
         right_top = sd.get_point(x + length_x, y + length_y)
         sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
+        left_bottom = sd.get_point(x+1, y+1)
+        right_top = sd.get_point(x + length_x -1, y + length_y -1)
+        sd.rectangle(left_bottom=left_bottom, right_top=right_top, color=sd.random_color(), width=0)
 sd.pause()

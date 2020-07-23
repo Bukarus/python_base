1b5ef971acfe79ed94ecd3ba5929f7146a8e0d98
Ruslan <rusikeabudana@gmail.com>
2020-07-23 23:24:20 +0600

    4.1-4.2 - выполнил

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -18,7 +18,7 @@ import simple_draw as sd
 #   текущая_точка = начальная точка
 #   для угол_наклона из диапазона от 0 до 360 с шагом XXX
 #      # XXX подбирается индивидуально для каждой фигуры
-#      составляем вектор из текущая_точка заданной длины с наклоном в угол_наклона
+#      составляем вектор тиз текущая_точка заданной длины с наклоном в угол_наклона
 #      рисуем вектор
 #      текущая_точка = конечной точке вектора
 #
@@ -36,7 +36,80 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+point_initial_triangle = sd.get_point(100, 300)
+point_initial_square = sd.get_point(300, 150)
+point_initial_pentagon = sd.get_point(500, 300)
+point_initial_hexagon = sd.get_point(150, 400)
+length_initial = 100
+angle_initial = 60
+
+
+def triangle(point=point_initial_triangle, angle=angle_initial, length=length_initial):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+    v3.draw()
+
+
+def square(point=point_initial_square, angle=angle_initial, length=length_initial):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+    v3.draw()
+
+    sd.line(start_point=v3.end_point, end_point=v1.start_point, width=3)
+
+
+def pentagon(point=point_initial_pentagon, angle=angle_initial, length=length_initial):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=3)
+    v5.draw()
+
+
+def hexagon(point=point_initial_hexagon, angle=angle_initial, length=length_initial):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+    v5.draw()
+
+    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+    v6.draw()
+
+
+triangle()
+square()
+pentagon()
+hexagon()
+
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,6 +14,127 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+COLOR_RED = sd.COLOR_RED
+COLOR_ORANGE = sd.COLOR_ORANGE
+COLOR_YELLOW = sd.COLOR_YELLOW
+COLOR_GREEN = sd.COLOR_GREEN
+COLOR_CYAN = sd.COLOR_CYAN
+COLOR_BLUE = sd.COLOR_BLUE
+COLOR_PURPLE = sd.COLOR_PURPLE
 
-sd.pause()
+
+colors = {
+    0: COLOR_RED,
+    1: COLOR_ORANGE,
+    2: COLOR_YELLOW,
+    3: COLOR_GREEN,
+    4: COLOR_CYAN,
+    5: COLOR_BLUE,
+    6: COLOR_PURPLE,
+}
+
+color_names = {
+    0: 'red',
+    1: 'orange',
+    2: 'yellow',
+    3: 'green',
+    4: 'cyan',
+    5: 'blue',
+    6: 'purple',
+}
+
+
+point_initial_triangle = sd.get_point(100, 300)
+point_initial_square = sd.get_point(300, 150)
+point_initial_pentagon = sd.get_point(500, 300)
+point_initial_hexagon = sd.get_point(150, 400)
+length_initial = 100
+angle_initial = 60
+user_color = COLOR_YELLOW
+
+
+def triangle(point=point_initial_triangle, angle=angle_initial, length=length_initial, color=user_color):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+    v2.draw(color=color)
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+    v3.draw(color=color)
+
+
+def square(point=point_initial_square, angle=angle_initial, length=length_initial, color=user_color):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+    v2.draw(color=color)
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+    v3.draw(color=color)
+
+    sd.line(start_point=v3.end_point, end_point=v1.start_point, width=3, color=color)
+
+
+def pentagon(point=point_initial_pentagon, angle=angle_initial, length=length_initial, color=user_color):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+    v2.draw(color=color)
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
+    v3.draw(color=color)
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
+    v4.draw(color=color)
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=3)
+    v5.draw(color=color)
+
+
+def hexagon(point=point_initial_hexagon, angle=angle_initial, length=length_initial, color=user_color):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw(color=color)
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+    v2.draw(color=color)
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+    v3.draw(color=color)
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+    v4.draw(color=color)
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+    v5.draw(color=color)
+
+    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+    v6.draw(color=color)
+
+
+print('Возможные цвета: ')
+for color_name in color_names:
+    print("{}: {}".format(color_name, color_names[color_name]))
+# print('0: red')
+# print('1: orange')
+# print('2: yellow')
+# print('3: green')
+# print('4: cyan')
+# print('5: blue')
+# print('6: puple')
+
+user_input = input("Введите, пожалуйста, желаемый цвет: ")
+user_color = int(user_input)
+
+while user_color in colors:
+    triangle(color=colors[user_color])
+    square(color=colors[user_color])
+    pentagon(color=colors[user_color])
+    hexagon(color=colors[user_color])
+    # sd.pause()
+    user_input = input("Введите, пожалуйста, желаемый цвет: ")
+    user_color = int(user_input)
+else:
+    print("Вы ввели некорректный номер!")

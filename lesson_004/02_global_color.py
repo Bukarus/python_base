c56495038930f0a468c9f4221a2216a26c8a2e3b
rusikeabudana <rusikeabudana@gmail.com>
2020-07-28 17:08:03 +0600

    4-2 - переделал словарь с индексами и именами

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -13,135 +13,77 @@ import simple_draw as sd
 # sd.get_vector()
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
-
-COLOR_RED = sd.COLOR_RED
-COLOR_ORANGE = sd.COLOR_ORANGE
-COLOR_YELLOW = sd.COLOR_YELLOW
-COLOR_GREEN = sd.COLOR_GREEN
-COLOR_CYAN = sd.COLOR_CYAN
-COLOR_BLUE = sd.COLOR_BLUE
-COLOR_PURPLE = sd.COLOR_PURPLE
-
-
-# TODO Создайте структуру данных вида:
-#  colors = {'1': ['красный', sd.COLOR_RED], ...}
-#  или так colors = {'1': {'name': 'красный', 'color': sd.COLOR_RED}, ...}
-#  Так будет удобнее добавлять, удалять менять местами цвета, если
-#  возникнет такая необходимость. А использование словаря поможет в выводе сообщений и
-#  обработке пользовательского ввода.
-colors = {
-    0: COLOR_RED,
-    1: COLOR_ORANGE,
-    2: COLOR_YELLOW,
-    3: COLOR_GREEN,
-    4: COLOR_CYAN,
-    5: COLOR_BLUE,
-    6: COLOR_PURPLE,
-}
-
-color_names = {
-    0: 'red',
-    1: 'orange',
-    2: 'yellow',
-    3: 'green',
-    4: 'cyan',
-    5: 'blue',
-    6: 'purple',
-}
-
-
 point_initial_triangle = sd.get_point(100, 300)
 point_initial_square = sd.get_point(300, 150)
 point_initial_pentagon = sd.get_point(500, 300)
 point_initial_hexagon = sd.get_point(150, 400)
 length_initial = 100
 angle_initial = 60
-user_color = COLOR_YELLOW
+user_color = sd.COLOR_YELLOW
 
 
-# TODO Нужно разместить функции над остальным кодом.
 def triangle(point=point_initial_triangle, angle=angle_initial, length=length_initial, color=user_color):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
-    v3.draw(color=color)
+    number_of_angles = 3
+    angle_delta = 180 - (number_of_angles - 2) * 180 / number_of_angles
+    end_point = point
+    for i in range(number_of_angles):
+        v = sd.get_vector(start_point=end_point, angle=angle + angle_delta * i, length=length, width=3)
+        end_point = v.end_point
+        v.draw(color=color)
 
 
 def square(point=point_initial_square, angle=angle_initial, length=length_initial, color=user_color):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
-    v3.draw(color=color)
-
-    sd.line(start_point=v3.end_point, end_point=v1.start_point, width=3, color=color)
+    number_of_angles = 4
+    angle_delta = 180 - (number_of_angles - 2) * 180 / number_of_angles
+    end_point = point
+    for i in range(number_of_angles):
+        v = sd.get_vector(start_point=end_point, angle=angle + angle_delta * i, length=length, width=3)
+        end_point = v.end_point
+        v.draw(color=color)
 
 
 def pentagon(point=point_initial_pentagon, angle=angle_initial, length=length_initial, color=user_color):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
-    v2.draw(color=color)
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
-    v3.draw(color=color)
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
-    v4.draw(color=color)
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=3)
-    v5.draw(color=color)
+    number_of_angles = 5
+    angle_delta = 180 - (number_of_angles - 2) * 180 / number_of_angles
+    end_point = point
+    for i in range(number_of_angles):
+        v = sd.get_vector(start_point=end_point, angle=angle + angle_delta * i, length=length, width=3)
+        end_point = v.end_point
+        v.draw(color=color)
 
 
 def hexagon(point=point_initial_hexagon, angle=angle_initial, length=length_initial, color=user_color):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color)
+    number_of_angles = 6
+    angle_delta = 180 - (number_of_angles - 2) * 180 / number_of_angles
+    end_point = point
+    for i in range(number_of_angles):
+        v = sd.get_vector(start_point=end_point, angle=angle + angle_delta * i, length=length, width=3)
+        end_point = v.end_point
+        v.draw(color=color)
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
-    v2.draw(color=color)
 
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
-    v3.draw(color=color)
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
-    v4.draw(color=color)
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
-    v5.draw(color=color)
-
-    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
-    v6.draw(color=color)
+color_names = {
+    '0': {'name': 'red', 'color': sd.COLOR_RED},
+    '1': {'name': 'orange', 'color': sd.COLOR_ORANGE},
+    '2': {'name': 'yellow', 'color': sd.COLOR_YELLOW},
+    '3': {'name': 'green', 'color': sd.COLOR_GREEN},
+    '4': {'name': 'cyan', 'color': sd.COLOR_CYAN},
+    '5': {'name': 'blue', 'color': sd.COLOR_BLUE},
+    '6': {'name': 'purple', 'color': sd.COLOR_DARK_PURPLE},
+}
 
 
 print('Возможные цвета: ')
 for color_name in color_names:
-    print("{}: {}".format(color_name, color_names[color_name]))
-# print('0: red')
-# print('1: orange')
-# print('2: yellow')
-# print('3: green')
-# print('4: cyan')
-# print('5: blue')
-# print('6: puple')
+    print("{}: {}".format(color_name, color_names[color_name]['name']))
 
-user_input = input("Введите, пожалуйста, желаемый цвет: ")
-user_color = int(user_input)
+user_color = input("Введите, пожалуйста, желаемый цвет: ")
 
-while user_color in colors:
-    triangle(color=colors[user_color])
-    square(color=colors[user_color])
-    pentagon(color=colors[user_color])
-    hexagon(color=colors[user_color])
-    # sd.pause()
-    user_input = input("Введите, пожалуйста, желаемый цвет: ")
-    user_color = int(user_input)
+while user_color in color_names:
+    triangle(color=color_names[user_color]['color'])
+    square(color=color_names[user_color]['color'])
+    pentagon(color=color_names[user_color]['color'])
+    hexagon(color=color_names[user_color]['color'])
+    user_color = input("Введите, пожалуйста, желаемый цвет: ")
 else:
     print("Вы ввели некорректный номер!")

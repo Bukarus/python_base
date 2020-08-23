5c2c3e5170fb2e7b2fc028f826aab018e01319c8
Ruslan <rusikeabudana@gmail.com>
2020-08-23 20:41:21 +0600

    5/1 - 5/2 - сделал 2 задания

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,4 +3,17 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+# from room_1 import folks as folks1, __name__ as name1
+# from room_2 import folks as folks2, __name__ as name2
+
+# str1 = "В комнате " + name1 + " живут: " + folks1[0] + " и " + folks1[1]
+# str2 = "В комнате " + name2 + " живут: " + folks2[0]
+
+import room_1 as r1
+import room_2 as r2
+
+str1 = "В комнате " + r1.__name__ + " живут: " + r1.folks[0] + " и " + r1.folks[1]
+str2 = "В комнате " + r2.__name__ + " живут: " + r2.folks[0]
+
+print(str1)
+print(str2)
--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,3 +6,24 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from district.central_street.house1 import room1 as ch1_r1, room2 as ch1_r2
+from district.central_street.house2 import room1 as ch2_r1, room2 as ch2_r2
+from district.soviet_street.house1 import room1 as sh1_r1, room2 as sh1_r2
+from district.soviet_street.house2 import room1 as sh2_r1, room2 as sh2_r2
+
+str = ", "
+str_total = "На районе живут: "
+help_list = []
+
+help_list.extend(ch1_r1.folks)
+help_list.extend(ch1_r2.folks)
+help_list.extend(ch2_r1.folks)
+help_list.extend(ch2_r2.folks)
+help_list.extend(sh1_r1.folks)
+help_list.extend(sh1_r2.folks)
+help_list.extend(sh2_r1.folks)
+help_list.extend(sh2_r2.folks)
+
+str_total += str.join(help_list)
+
+print(str_total)

211d624abf6dc911213f9b04e9713f580b935ed9
rusikeabudana <rusikeabudana@gmail.com>
2020-08-26 15:19:33 +0600

    убрал TODO со 2 задания

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,7 +5,6 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
 from district.central_street.house1 import room1 as ch1_r1, room2 as ch1_r2
 from district.central_street.house2 import room1 as ch2_r1, room2 as ch2_r2
 from district.soviet_street.house1 import room1 as sh1_r1, room2 as sh1_r2

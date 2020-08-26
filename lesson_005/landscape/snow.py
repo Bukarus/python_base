95df1b2a82f35f28a68a901f5ebcf6c0e14ba360
rusikeabudana <rusikeabudana@gmail.com>
2020-08-26 16:10:18 +0600

    5.4 - модуль с сугробом

---
+++ lesson_005/landscape/snow.py
@@ -0,0 +1,32 @@
+import simple_draw as sd
+
+
+def snowflow():
+    sd.resolution = (1200, 600)
+
+    N = 100
+
+    snowflakes = {}
+    for i in range(N):
+        snowflakes[i] = {}
+        snowflakes[i]['x'] = sd.random_number(0, 300)
+        snowflakes[i]['y'] = 10
+        snowflakes[i]['length'] = sd.random_number(5, 15)
+        snowflakes[i]['factor_a'] = sd.random_number(1, 8)/10
+        snowflakes[i]['factor_b'] = sd.random_number(1, 8)/10
+        snowflakes[i]['factor_c'] = sd.random_number(30, 60)
+        snowflakes[i]['speed'] = sd.random_number(5, 20)
+
+    flag_of_stop = False
+
+    while True:
+        for i, snowflake_item in snowflakes.items():
+             point = sd.get_point(snowflake_item['x'], snowflake_item['y'])
+             sd.snowflake(center=point, length=snowflake_item['length'], factor_a=snowflake_item['factor_a'],
+                          factor_b=snowflake_item['factor_b'], factor_c=snowflake_item['factor_c'])
+        if sd.user_want_exit():
+            break
+
+
+snowflow()
+sd.pause()

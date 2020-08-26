f8b2e0bd8013f1f942f3efedeeacb76310542d5c
rusikeabudana <rusikeabudana@gmail.com>
2020-08-26 15:00:01 +0600

    добавил новый модуль my_burger

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,24 @@
+def roll_add_below():
+    print("Возьмем булочку!")
+
+def roll_add_above():
+    print("Накроем булочкой сверху!")
+
+
+def cutlet_add():
+    print("А теперь добавим котлету!")
+
+def cucumber_add():
+    print("А теперь добавим огурчик!")
+
+def tomato_add(number=1):
+    print("А теперь добавим помидорчик")
+
+def mayonnaise_add():
+    print("А теперь добавим майонез!")
+
+def cheese_add():
+    print("А теперь добавим сыр!")
+
+def sausage_add():
+    print("А теперь добавим колбаски!")
